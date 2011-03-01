34026cb06a93f2e4c777e91a29a480fd5aaeb5db
Marek Kubis <marek.kubis@gmail.com>
2011-03-01 23:47:35 +0000

    xml emitter update

--- src/main/scala/org/wquery/emitter/XmlWQueryEmitter.scala
+++ src/main/scala/org/wquery/emitter/XmlWQueryEmitter.scala
@@ -1,4 +1,6 @@
 package org.wquery.emitter
+
+import org.wquery.model.Arc
 import org.wquery.engine.{Result, Error, Answer}
 import org.wquery.model.{Sense, Synset, DataSet}
 
@@ -14,37 +16,74 @@ class XmlWQueryEmitter extends WQueryEmitter {
   
   private def emitDataSet(dataSet: DataSet): String = {
     val builder = new StringBuilder
-    builder append "<RESULT>\n" 
+    val pathVarNames = dataSet.pathVars.keys.toSeq.filterNot(_.startsWith("_")).sortWith((x, y) => x < y)
+    val stepVarNames = dataSet.stepVars.keys.toSeq.filterNot(_.startsWith("_")).sortWith((x, y) => x < y)
+    
+    builder append "<RESULTS>\n"
+            
+    for (i <- 0 until dataSet.pathCount ) {
+      builder append "<RESULT>\n"        
+        
+      val tuple = dataSet.paths(i)      
+    
+      if (!pathVarNames.isEmpty || !stepVarNames.isEmpty) {
+        builder append "<BINDINGS>\n"  
+          
+        for (pathVarName <- pathVarNames) {
+          val varPos = dataSet.pathVars(pathVarName)(i)   
+            
+          builder append "<PATHVAR name=\""
+          builder append pathVarName
+          builder append "\">\n"
+          emitTuple(tuple.slice(varPos._1, varPos._2), builder)
+          builder append "</PATHVAR>\n"
+        }
+        
+        for (stepVarName <- stepVarNames) {
+          builder append "<STEPVAR name=\""
+          builder append stepVarName
+          builder append "\">\n"
+          emitElement(tuple(dataSet.stepVars(stepVarName)(i)), builder)
+          builder append "</STEPVAR>\n"
+        }
+      }
       
-    dataSet.paths.foreach { tuple => 
-      builder append "<TUPLE>\n"
-      tuple.foreach {elem => emitElement(elem, builder)}
-      builder append "</TUPLE>\n"        
-    }
-  
-    builder append "</RESULT>"    
+      builder append "</BINDINGS>\n"                
+      emitTuple(tuple, builder)
+      builder append "</RESULT>\n"
+    }    
+
+    builder append "</RESULTS>\n"    
     builder.toString
   }
+    
+  private def emitTuple(tuple: List[Any], builder: StringBuilder) {
+    builder append "<TUPLE>\n"    
+    tuple.foreach(emitElement(_, builder))    
+    builder append "</TUPLE>\n"    
+  }        
+        
   
   private def emitElement(element: Any, builder: StringBuilder) {
     element match {
       case element: Synset =>
-        builder append "<SYNSET>\n"
-        
-        element.senses.foreach{ sense => 
-          emitSense(sense, builder)
-          builder append " "
-        }
-        
+        builder append "<SYNSET>\n"        
+        element.senses.foreach(emitSense(_, builder))        
         builder append "</SYNSET>\n"        
       case element: Sense => 
         emitSense(element, builder)      
+      case element: Arc =>
+        emitArc(element, builder)
       case element => 
         builder append "<VALUE>" append element append "</VALUE>\n"      
     }
   }
   
   private def emitSense(sense: Sense, builder: StringBuilder) {
-    builder append "<LITERAL sense=\"" append sense.senseNumber append "\">" append sense.wordForm append "</ILR>\n" 
+    builder append "<LITERAL sense=\"" append sense.senseNumber append "\">" append sense.wordForm append "</LITERAL>\n" 
   }
+  
+  private def emitArc(arc: Arc, builder: StringBuilder) {    
+    builder append "<ARC from=\"" append arc.from append "\" to=\"" append arc.to append "\">" append arc.relation.name append "</ARC>\n" 
+  }  
 }
