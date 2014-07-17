dbf39bd5a3c7cd315bda89b02b87bddb32916045
Marek Kubis <marek.kubis@gmail.com>
2014-07-17 21:55:38 +0200

    version number in the wordnet object for the purpose of serialization

--- src/main/scala/org/wquery/model/impl/InMemoryWordNet.scala
+++ src/main/scala/org/wquery/model/impl/InMemoryWordNet.scala
@@ -2,14 +2,14 @@ package org.wquery.model.impl
 
 import org.wquery.model._
 import org.wquery.path.operations.NewSynset
-import org.wquery.{WQueryModelException, WQueryUpdateBreaksRelationPropertyException}
+import org.wquery.{WQueryModelException, WQueryProperties, WQueryUpdateBreaksRelationPropertyException}
 
 import scala.collection.mutable.{ListBuffer, Map => MMap}
 import scala.concurrent.stm._
 import scalaz.Scalaz._
-import scalaz._
 
 class InMemoryWordNet extends WordNet {
+  private val version = WQueryProperties.version // for the purpose of serialization
   private val successors = MMap[Relation, MMap[(String, Any), Vector[Map[String, Any]]]]()
   private val aliasMap = scala.collection.mutable.Map[Relation, List[Arc]]()
   private var relationsList = List[Relation]()
