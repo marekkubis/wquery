4be3de6e8de80f4809b876db8427c73961be22e4
Marek Kubis <marek.kubis@gmail.com>
2015-08-16 02:22:04 +0200

    path measure

---
+++ src/main/scala/org/wquery/similarity/WSimMain.scala
@@ -0,0 +1,101 @@
+package org.wquery.similarity
+
+import java.io._
+
+import org.rogach.scallop._
+import org.rogach.scallop.exceptions.{Help, ScallopException, Version}
+import org.wquery.WQueryProperties
+import org.wquery.emitter.TsvWQueryEmitter
+import org.wquery.loader.WnLoader
+import org.wquery.update.WUpdate
+
+object WSimMain {
+  val loader = new WnLoader
+  val emitter = new TsvWQueryEmitter
+
+  def main(args: Array[String]) {
+    val opts = Scallop(args)
+      .version("wsim " + WQueryProperties.version + " " + WQueryProperties.copyright)
+      .banner( """
+                 |Computes semantic similarity among pairs of words, senses and synsets.
+                 |
+                 |usage:
+                 |
+                 |  wsim [OPTIONS] [WORDNET] [IFILE] [OFILE]
+                 |
+                 |options:
+                 | """.stripMargin)
+      .opt[Boolean]("help", short = 'h', descr = "Show help message")
+      .opt[Boolean]("version", short = 'v', descr = "Show version")
+      .opt[String]("measure", short = 'm', default = () => Some("path"),
+        descr = "Similarity measure")
+      .trailArg[String](name = "WORDNET", required = false,
+        descr = "A wordnet model as created by wcompile (read from stdin if not specified)")
+      .trailArg[String](name = "IFILE", required = false,
+        descr = "Tab separated pairs of words, senses or synsets (read from stdin if not specified)")
+      .trailArg[String](name = "OFILE", required = false,
+        descr = "Similarity values (printed to stdout if not specified)")
+
+    try {
+      opts.verify
+
+      val measure = opts[String]("measure")
+
+      val wordNetInput = opts.get[String]("WORDNET")
+        .map(inputName => new FileInputStream(inputName))
+        .getOrElse(System.in)
+
+      val wupdate = new WUpdate(loader.load(wordNetInput))
+
+      val input = opts.get[String]("IFILE")
+        .map(ifile => new FileInputStream(ifile))
+        .getOrElse(System.in)
+
+      val output = opts.get[String]("OFILE")
+        .map(outputName => new BufferedOutputStream(new FileOutputStream(outputName)))
+        .getOrElse(System.out)
+
+      val writer = new BufferedWriter(new OutputStreamWriter(output))
+
+      writer.write(emitter.emit(wupdate.execute(
+        """
+          |function min_path_length do
+          |   %l := %A$a$_<$a>
+          |   %r := %A$a<$a>
+          |   %p := shortest({%l}.hypernym*.^hypernym*.{%r})
+          |
+          |   if [ empty(%p) ] do
+          |     %ll := min(size(shortest({%l}.hypernym*[empty(hypernym)])))
+          |     %rl := min(size(shortest({%r}.hypernym*[empty(hypernym)])))
+          |     emit distinct(%ll + %rl + 1)
+          |   end else
+          |     emit distinct(size(%p))
+          |end
+        """.stripMargin)))
+
+      writer.write(emitter.emit(wupdate.execute(
+        """
+          |function path_measure do
+          |   emit 1/min_path_length(%A)
+          |end
+        """.stripMargin)))
+
+      for (line <- scala.io.Source.fromInputStream(input).getLines()) {
+        val result = wupdate.execute(measure + "_measure(as_tuple(`" + line + "`, `/ /`))")
+        writer.write(emitter.emit(result))
+      }
+
+      writer.close()
+    } catch {
+      case e: Help =>
+        opts.printHelp()
+      case Version =>
+        opts.printHelp()
+      case e: ScallopException =>
+        println("ERROR: " + e.message)
+        println()
+        opts.printHelp()
+        sys.exit(1)
+    }
+  }
+}
