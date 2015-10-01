c52d0da9366220649c8b6063c4e514a51d4d8f04
Marek Kubis <marek.kubis@gmail.com>
2015-10-01 02:59:01 +0200

    wup measure compatibility revised

--- src/test/scala/org/wquery/MeasuresTestSuite.scala
+++ src/test/scala/org/wquery/MeasuresTestSuite.scala
@@ -31,9 +31,9 @@ class MeasuresTestSuite extends WQueryTestSuite {
 
   @Test def testLchMeasure() = result of "lch_measure({bus:4:n}, {taxi:1:n})" should equal ("2.159484249353372\n")
 
-  @Test def testRootDistForCar() = result of "root_dist({car:1:n})" should equal ("12\n")
+  @Test def testRootDistForCar() = result of "root_dist({car:1:n})" should equal ("11\n")
 
-  @Test def testRootDistForBus() = result of "root_dist({'motor vehicle':1:n})" should equal ("11\n")
+  @Test def testRootDistForBus() = result of "root_dist({'motor vehicle':1:n})" should equal ("10\n")
 
   @Test def testLcsDistForBus() = result of "lcs_dist({bus:4:n}, {'motor vehicle':1:n})" should equal ("2\n")
 
@@ -41,9 +41,9 @@ class MeasuresTestSuite extends WQueryTestSuite {
 
   @Test def testLcsDistForMotorVehicle() = result of "lcs_dist({'motor vehicle':1:n}, {'motor vehicle':1:n})" should equal ("0\n")
 
-  @Test def testWupMeasure() = result of "wup_measure({bus:4:n}, {taxi:1:n})" should startWith ("0.9230769230769231")
+  @Test def testWupMeasure() = result of "wup_measure({bus:4:n}, {taxi:1:n})" should startWith ("0.9166666666666666")
 
-  @Test def testWupMeasureOnHypernymyPath() = result of "wup_measure({bus:4:n}, {car:1:n})" should equal ("0.96\n")
+  @Test def testWupMeasureOnHypernymyPath() = result of "wup_measure({bus:4:n}, {car:1:n})" should startWith ("0.9565217391304348")
 
   @Test def testWupMeasureOnEqualSynsets() = result of "wup_measure({car:1:n}, {auto:1:n})" should equal ("1.0\n")
 
